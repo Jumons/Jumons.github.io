d799edb5c8557f2ff01acb386042f2582a9438c5
Jumons <mon080921@gmail.com>
2020-04-12 00:39:20 +0900

    fix indexed bug

--- visualizer/vis.js
+++ visualizer/vis.js
@@ -16,7 +16,7 @@ function visualize() {
     // let nodes = new vis.DataSet([...Array(node_num)].map((a,b) => Object({id: b+1, label: String(b+1)})));
     let nodes = [];
     for (var i = 0; i < node_num; i++) {
-        let idx = i + (labeled ? 1 : 0);
+        let idx = i + (one_indexed ? 0 : 1);
         let node = {
             id: idx,
             label: String(idx),
